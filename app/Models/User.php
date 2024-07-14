77b72962acfe27401219489a122b10eb7bbeb8ac
danyk <danykdev@gmail.com>
2024-07-14 14:05:05 +0300

    add form for submitting a job

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+      'first_name',
+      'last_name',
+      'email',
+      'password',
     ];
 
     /**
@@ -29,8 +30,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+      'password',
+      'remember_token',
+      'admin'
     ];
 
     /**
@@ -39,7 +41,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
     ];
 }
